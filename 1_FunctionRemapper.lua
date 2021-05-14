8fc17ac979efd5862ac72bd4865cebf6bebbc607
Jasen Samuels <jasensamuels@gmail.com>
2021-05-13 21:08:47 -0400

    Update for LuaPlugin version 5.1 or newer which fixes inconsistencies with the "_0x" game native function calls and makes it even easier to work or do things with in a more consistent manner

--- 1_FunctionRemapper.lua
+++ 1_FunctionRemapper.lua
@@ -1,6 +1,6 @@
 return {
 	init	=	function()
-					if not JM36_GTAV_LuaPlugin_Version or JM36_GTAV_LuaPlugin_Version < 5.0 then error("You are attempting to use an incompatible or an outdated version of Lua Plugin with Function (Re)Mapper; please update to the latest version of JM36 GTAV Lua Plugin first - https://github.com/JayMontana36/LuaPlugin-GTAV/releases") return end
+					if not JM36_GTAV_LuaPlugin_Version or JM36_GTAV_LuaPlugin_Version < 5.1 then error("You are attempting to use an incompatible or an outdated version of Lua Plugin with Function (Re)Mapper; please update to the latest version of JM36 GTAV Lua Plugin first - https://github.com/JayMontana36/LuaPlugin-GTAV/releases") return end
 					local FunctionsToRemap, _G, _G2 = configFileRead("1_FunctionRemapper.ini"), _G, _G2
 					for k,v in pairs(FunctionsToRemap) do
 						_G2[k] = _G[v]
